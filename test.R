bc32f261316e6c4da355c92c43c543b129d74167
Lubomír Seif <seiflubomir@gmail.com>
2023-10-22 11:36:33 +0200

    added spaces in indexing

--- test.R
+++ test.R
@@ -22,7 +22,7 @@ fibonacci <- function(n) {
         fib[1] <- 0
         fib[2] <- 1
         for (i in 3:n) {
-            fib[i] <- fib[i-1] + fib[i-2]
+            fib[i] <- fib[i - 1] + fib[i - 2]
         }
         return(fib)
     }
