9e0d3a2f7ae55276981d14a8678977c89a5a9c20
Lubomír Seif <seiflubomir@gmail.com>
2023-10-22 11:25:34 +0200

    added function for fibonacci sequence

--- test.R
+++ test.R
@@ -10,3 +10,23 @@ print(summary(data))
 print("test pull req")
 
 # xd nevim lol
+
+# Generate Fibonacci sequence without recursion
+fibonacci <- function(n) {
+    if (n == 0) {
+        return(0)
+    } else if (n == 1) {
+        return(1)
+    } else {
+        fib <- numeric(n)
+        fib[1] <- 0
+        fib[2] <- 1
+        for (i in 3:n) {
+            fib[i] <- fib[i-1] + fib[i-2]
+        }
+        return(fib)
+    }
+}
+
+# Test the function
+print(fibonacci(10))
